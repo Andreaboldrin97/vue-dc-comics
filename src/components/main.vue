a873d360f35285b90f631c665cfe6a49899a8707
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-15 18:23:49 +0200

    change padding shopper section

--- src/components/main.vue
+++ src/components/main.vue
@@ -5,23 +5,23 @@
       </div>
       <section>
         <div class="shopper d-flex p-5 text-white">
-            <div class="pe-3">
+            <div class="pe-4">
                 <img src="../assets/img/buy-comics-digital-comics.png" alt="">
                 DIGITAL COMICS
             </div>
-            <div class="pe-3">
+            <div class="pe-4">
                 <img src="../assets/img/buy-comics-merchandise.png" alt="">
                 DC MERCHANDISE
             </div>
-            <div class="pe-3">
+            <div class="pe-4">
                 <img src="../assets/img/buy-comics-shop-locator.png" alt="">
                 COMIC SHOP LOCATOR
             </div>
-            <div class="pe-3">
+            <div class="pe-4">
                 <img src="../assets/img/buy-comics-subscriptions.png" alt="">
                 SUBSCRIPTIONS
             </div>
-            <div class="pe-3">
+            <div class="pe-4">
                 <img src="../assets/img/buy-dc-power-visa.svg" alt="">
                 DC POWER VISA
             </div>
