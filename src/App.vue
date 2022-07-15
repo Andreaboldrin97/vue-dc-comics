7b378b787ceb6a7bcc0448ae64727163412876f7
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-15 14:58:14 +0200

    create styles folder

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,9 @@ export default {
 </script>
 
 <style lang="scss">
-@import "~/node_modules/bootstrap/scss/bootstrap.scss"
+@import "~/node_modules/bootstrap/scss/bootstrap.scss";
+@import "./styles/general.scss";
+@import "./styles/general.scss";
+
 
 </style>
