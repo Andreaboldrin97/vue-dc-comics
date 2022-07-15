833b8d12bab332490d68ae30459b56a109bcf10e
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-15 14:47:24 +0200

    add vue cli link

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <headerMain/>
+  </div>
+</template>
+
+<script>
+import headerMain from './components/header.vue'
+import mainContent from './components/main.vue'
+import footerMain from './components/footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    headerMain ,
+    mainContent,
+    footerMain,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
