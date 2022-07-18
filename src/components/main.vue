fe800c41265b6f1d876a3c5bbd91cf55bdf08c91
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-18 15:52:17 +0200

    add btn-series

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,7 +1,10 @@
 <template>
     <main class="bg-dark">
-        <div class="hero_series">
+        <div class="hero_series position-relative">
             <img  src="../assets/img/jumbotron.jpg" alt="">
+            <div class="px-5 position-absolute bottom-0 start-0 container-btn-series">
+                <span class="btn-series px-4 py-2 mx-4"><a class="text-white" href="">CURRENT SERIES</a></span>
+            </div>
         </div>
         <div class="text-white px-5 py-5 container-fluid">
             <div class="px-5 row" >
@@ -116,8 +119,15 @@ comicsShop,
         width: 100%;
     }
 }
-.btn-load-more{
+.btn-load-more,
+.btn-series{
     background: $colorBreand;
+}
+.container-btn-series{
+    transform: translateY(50%);
+        a{
+        text-decoration: none;
+        }
 }
 
 </style>
